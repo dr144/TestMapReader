0764800d8b53e8641afb2ee9884caa7168ffe377
dr144 <kevin@locusfocuscamera.com>
2023-11-04 12:35:43 -0500

    Initial Commit

---
+++ TestMapReader/TestMapReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestMapReaderApp.swift
+//  TestMapReader
+//
+//  Created by Kevin Gross on 11/4/23.
+//
+
+import SwiftUI
+
+@main
+struct TestMapReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
